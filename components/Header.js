2c04da9b79c0a25eff096ef2eb67eabf960fd30e
ravi kunwar <ravikunwar079@gmail.com>
2021-07-12 20:31:16 +0530

    hulu clone with next js

---
+++ components/Header.js
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import hululogo from '../public/images/hululogo.png'
+import HeaderItem from './HeaderItem';
+import { 
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon 
+    } from '@heroicons/react/outline'
+
+function Header() {
+    return (
+        <header className="flex flex-col sm:flex-row m-5 justify-between 
+                            items-center h-auto ">
+            <div className="w-full sm:w-auto flex flex-grow items-center justify-evenly 
+                            max-w-2xl mt-5 overflow-scroll scrollbar-hide" >
+                <HeaderItem title="HOME" Icon={HomeIcon}/>
+                <HeaderItem title="TRENDING" Icon={LightningBoltIcon}/>
+                <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon}/>
+                <HeaderItem title="COLLECTIONS" Icon={CollectionIcon}/>
+                <HeaderItem title="SEARCH" Icon={SearchIcon}/>
+                <HeaderItem title="ACCOUNT" Icon={UserIcon}/>
+            </div>
+            <Image 
+                src={hululogo} 
+                height={90} 
+                width={140}
+                className="object_contain"/>
+                
+        </header>
+    )
+}
+
+export default Header
+
+
+
